09e32f3c4cea65c59b21b36f54e6c148d56166df
julianamedeiros <juliana.652@gmail.com>
2023-11-24 15:20:34 +0100

    COMMIT

--- secondary.py
+++ secondary.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
 import time
 
 #access the website
@@ -9,14 +11,16 @@ driver.get("https://www.pracuj.pl/")
 
 #deal with popups
 cookies = driver.find_element(By.CSS_SELECTOR, '[data-test="button-submitCookie"]')
-cookies.click()
+cookies.click() #action.click(cookies)
 print('button clicked')
 
 #insert the info
-search = driver.find_element(By.CLASS_NAME, 'core_fhefgxl')
+search = driver.find_element(By.CSS_SELECTOR, '[data-test="input-field"]')
 search.send_keys('data analyst warsaw')
 print('info inserted')
-search.button = driver.find_element(By.CLASS_NAME, 'core_b1fqykql')
+
+#click search button
+search.button = driver.find_element(By.CLASS_NAME, 'size-large.variant-primary.core_b1fqykql')
 search.button.click()
 print('button clicked')
 
